aae7b40e48db114076d8b689a05abdd55c5a700f
Jakob Pålsson <jakob@rint.se>
2025-09-18 11:00:25 +0200

    Try setting bluetooth pin

--- Firmware/RTK_Everywhere/Bluetooth.ino
+++ Firmware/RTK_Everywhere/Bluetooth.ino
@@ -488,9 +488,9 @@ void bluetoothStart()
         // See issue: https://github.com/sparkfun/SparkFun_RTK_Firmware/issues/5
         // https://github.com/espressif/esp-idf/issues/1541
         //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-        /*
-        // Note: Since version 3.0.0 this library does not support legacy pairing (using fixed PIN consisting of 4
-        digits). esp_bt_sp_param_t param_type = ESP_BT_SP_IOCAP_MODE;
+        
+        // Note: Since version 3.0.0 this library does not support legacy pairing (using fixed PIN consisting of 4digits). 
+        esp_bt_sp_param_t param_type = ESP_BT_SP_IOCAP_MODE;
 
         esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_NONE; // Requires pin 1234 on old BT dongle, No prompt on new BT dongle
         // esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_OUT; //Works but prompts for either pin (old) or 'Does this 6 pin
@@ -505,7 +505,7 @@ void bluetoothStart()
         pin_code[2] = '3';
         pin_code[3] = '4';
         esp_bt_gap_set_pin(pin_type, 4, pin_code);
-        */
+        
         //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
         if (settings.bluetoothRadioType == BLUETOOTH_RADIO_SPP_AND_BLE)
